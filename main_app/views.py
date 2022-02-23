21e000264e73d2feaf6381689186075a95a2a734
Roby-diamond <martinez.robyp@gmail.com>
2022-02-23 09:52:32 -0700

    adds authorization

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from ast import Del
+from cmath import log
 from django.shortcuts import render, redirect
 from .models import Post, Photo
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
 
@@ -14,14 +19,17 @@ BUCKET = 'photodeck'
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def posts_index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(user=request.user)
     return render(request, 'posts/index.html', {'posts': posts})
 
+@login_required
 def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'posts/detail.html', {'post': post})
 
+@login_required
 def add_photo(request, post_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
@@ -36,14 +44,31 @@ def add_photo(request, post_id):
             print('An error occurred uploaded photo')
     return redirect('detail', post_id=post_id)
 
-class PostCreate(CreateView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Username or password is incorrect. Please try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ('title', 'description',)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ('title', 'description',)
 
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ('title', 'description',)
-
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = '/posts/'
