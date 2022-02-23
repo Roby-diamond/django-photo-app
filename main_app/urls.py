4432d3aee80ecfc06afd83f607745c7ecc89692f
Roby-diamond <martinez.robyp@gmail.com>
2022-02-23 08:29:36 -0700

    delete and update views functional

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('posts/', views.posts_index, name='index'),
     path('posts/<int:post_id>/', views.posts_detail, name='detail'),
-    path('posts/create/', views.PostCreate.as_view(), name='posts_create')
-    # path('profile/', views.upload_photo, name='upload_photo'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete', views.PostDelete.as_view(), name='posts_delete'),
+    path('posts/<int:post_id>/add_photo', views.add_photo, name='add_photo'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
+from ast import Del
 from django.shortcuts import render, redirect
-from .models import Post
-from django.views.generic.edit import CreateView
-from django.views.generic import ListView
+from .models import Post, Photo
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 import uuid
 import boto3
 
 
-# S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
-# BUCKET = 'photodeck'
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'photodeck'
+
 # Create your views here.
 
 def home(request):
@@ -21,6 +22,28 @@ def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'posts/detail.html', {'post': post})
 
+def add_photo(request, post_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, post_id=post_id)
+            photo.save()
+        except:
+            print('An error occurred uploaded photo')
+    return redirect('detail', post_id=post_id)
+
 class PostCreate(CreateView):
     model = Post
     fields = ('title', 'description',)
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ('title', 'description',)
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/posts/'
